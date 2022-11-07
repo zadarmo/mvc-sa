87998ba281a4d56fc961b4a50ae336dc5c67d42d
zadarmo <wx01290804@163.com>
2022-11-07 10:16:52 +0800

    echarts测试demo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import EchartsCpnDemo from "../example/EchartsCpnDemo.vue";
 </script>
 
-<template>123</template>
+<template>
+  <EchartsCpnDemo></EchartsCpnDemo>
+</template>
 
 <style scoped>
 .logo {
