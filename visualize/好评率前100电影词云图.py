5697a9f2e2b6229cb39ebefc6f8ba7ce6e6d5011
zadarmo <wx01290804@163.com>
2022-11-16 10:37:59 +0800

    过滤掉出现次数 < 30的评论

--- visualize/好评率前100电影词云图.py
+++ visualize/好评率前100电影词云图.py
@@ -9,14 +9,16 @@ df = pd.read_excel("./result/好评率前100电影的词频统计.xlsx")
 word_freq_str = str(df['pos_comment_word_json'][0])
 word_freq_json = word_freq_str + '}'
 m = json.loads(word_freq_json)
+sorted_m = sorted(m.items(), key=lambda x : x[1], reverse=True)
+filtered_sorted_m = list(filter(lambda x: x[1] >= 30, sorted_m))  # 过滤掉出现次数 < 30的评论
 
 data = []
 filter_word = ['电影', '剧情', '故事', '影片']
-for k, v in m.items():
+for k, v in filtered_sorted_m:
     if k in filter_word:
         continue
     data.append((k, v))
 mywordcloud = WordCloud()
-mywordcloud.add('',data, shape=SymbolType.DIAMOND)
+mywordcloud.add('',data, shape=SymbolType.ROUND_RECT)
 mywordcloud.set_global_opts(title_opts=opts.TitleOpts(title="好评率前100电影词云图", pos_left=330))
 mywordcloud.render('./figures/好评率前100电影词云图.html')
--- visualize/差评率前100电影词云图.py
+++ visualize/差评率前100电影词云图.py
@@ -8,14 +8,16 @@ from pyecharts.globals import SymbolType
 df = pd.read_excel("./result/差评率前100电影的词频统计.xlsx")
 word_freq_str = str(df['neg_comment_word_json'][0])
 m = json.loads(word_freq_str)
+sorted_m = sorted(m.items(), key=lambda x : x[1], reverse=True)
+filtered_sorted_m = list(filter(lambda x: x[1] >= 30, sorted_m))  # 过滤掉出现次数 < 30的评论
 
 data = []
 filter_word = ['电影', '剧情', '故事']
-for k, v in m.items():
+for k, v in filtered_sorted_m:
     if k in filter_word:
         continue
     data.append((k, v))
 mywordcloud = WordCloud()
-mywordcloud.add('',data, shape=SymbolType.DIAMOND)
+mywordcloud.add('',data, shape=SymbolType.ROUND_RECT)
 mywordcloud.set_global_opts(title_opts=opts.TitleOpts(title="差评率前100电影词云图", pos_left=330))
 mywordcloud.render('./figures/差评率前100电影词云图.html')
